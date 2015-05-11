504ddafe9c3c060340fb5aedf873bc96da9e51e5
natali-lapteva <nataliburt@gmail.ru>
2015-05-11 14:56:19 +0500

    Функция "Зашифровать".  Вывод зашифрованного сообщения по 6 символов. Вывод сообщения полбзователю.

--- theCaesarCipher/encript.js
+++ theCaesarCipher/encript.js
@@ -2,15 +2,24 @@ var alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ
 
 function encript () {
     var key = parseInt($('#key').val(), 10);
+        if (isNaN(key)) {
+            $('.msgToUserVvediteChislo').css('visibility', 'visible');
+        }
     console.log(key);
-    var inText = $('#inText').val().toUpperCase();
+    var inText = $('#inText').val().toUpperCase().replace(/Ё/g, 'Е');
     console.log(inText);
     var outText = [];
+    var spaceCounter = 0;
     for (var i = 0; i < inText.length; i++) {
         var index = alphabet.indexOf(inText[i]);
         if (index >= 0 ) {
-            outText.push(alphabet[index + key]);
+            outText.push(alphabet[(index + key + alphabet.length) % alphabet.length]);
+            spaceCounter++;
+            if (spaceCounter % 6 === 0) {
+                outText.push(' ');
+            }
         }
     };
+    $('#outputText').val(outText.join(''));
     console.log(outText.join(''));
 }
